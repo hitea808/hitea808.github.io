ce980e5605d704da9ed90bfcac776fb84c10258f
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2024-05-18 09:17:12 -1000

    navbar implementation

--- my-app/src/components/NavbarComponent.js
+++ my-app/src/components/NavbarComponent.js
@@ -3,25 +3,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import logo from '../img/logo.png';
+import Button from 'react-bootstrap/Button';
 
 import {
-
+    Instagram,
+    Envelope
 } from "react-bootstrap-icons";
 import "../css/NavbarComponent.css";
 
 const NavbarComponent = () => {
     return (
         <Navbar id="navbar" fixed="top">
-            <Row className="w-100 justify-content-evenly">
+            <Row className="w-100 justify-content-evenly align-items-center">
                 <Col className="text-center">
-                    <h1>icons</h1>
+                    <Instagram size={30} /> {' '}
+                    <Envelope size={35} />
                 </Col>
                 <Col className="text-center">
-                    <Image src="img/logo.png" rounded />
-                    {/* my-app/src/img/logo.png */}
+                    <Image id="logo" src={logo} alt="Logo"/>
                 </Col>
                 <Col className="text-center">
-                    <h1>order now</h1>
+                <button className="sec-color">ORDER ONLINE</button>
                 </Col>
             </Row>
         </Navbar>
